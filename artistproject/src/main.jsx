13b54eedfc38e00f64efda5da178952ffc494d28
DannyTan8x <dannytan8xtw@icloud.com>
2024-09-26 11:36:16 +0800

    add Boostrap, SCSS, Jquery

--- artistproject/src/main.jsx
+++ artistproject/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+// import "./index.css";
+import "./SCSS/all.scss";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
