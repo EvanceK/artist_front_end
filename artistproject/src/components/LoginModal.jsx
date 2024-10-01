6f2f1f268ba06050ab874982eefb2f25318805de
DannyTan8x <dannytan8xtw@icloud.com>
2024-10-01 22:08:44 +0800

    navbar update

--- artistproject/src/components/LoginModal.jsx
+++ artistproject/src/components/LoginModal.jsx
@@ -2,7 +2,7 @@ export default function LoginModal() {
   return (
     <div
       className="modal fade"
-      id="staticBackdrop"
+      id="LoginModal"
       data-bs-backdrop="static"
       data-bs-keyboard="false"
       tabIndex="-1"
@@ -22,8 +22,8 @@ export default function LoginModal() {
               aria-label="Close"
             ></button>
           </div>
-          <div className="login">
-            <div className="logo">Artis LOGO</div>
+          {/* <div className="login row">
+            <div className="h2 ">Artis LOGO</div>
             <form>
               <div className="input">
                 <h2>Sign Up or Log In</h2>
@@ -51,10 +51,50 @@ export default function LoginModal() {
                 </div>
               </div>
               <div className="btngroup">
-                <div className="btn btn-secondary" data-bs-dismiss="modal">
+                <div className="btn" data-bs-dismiss="modal">
                   LOG IN
                 </div>
                 <div className="btn btn-primary">SIGN UP</div>
+              </div>
+            </form>
+          </div> */}
+          <div className="login row">
+            <div className="h2 my-3 ">Artis LOGO</div>
+            <form>
+              <div className="input m-x-3">
+                <h2 className="d-flex m-5 ">Sign Up or Log In</h2>
+                <div className="d-block">
+                  <div className="row mt-5 m-2">
+                    <label className="col-3" htmlFor="email">
+                      Email:
+                    </label>
+                    <input
+                      id="email"
+                      className="col-8"
+                      name="email"
+                      type="text"
+                      placeholder="email"
+                    />
+                  </div>
+                  <div className="row m-2">
+                    <label className="col-3" htmlFor="password">
+                      Password:
+                    </label>
+                    <input
+                      id="password"
+                      className="col-8"
+                      name="password"
+                      type="text"
+                      placeholder="password"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="row my-5 mx-auto justify-content-center">
+                <div className="btn col-3 mx-2" data-bs-dismiss="modal">
+                  LOG IN
+                </div>
+                <div className="btn btn-primary col-3 mx-2">SIGN UP</div>
               </div>
             </form>
           </div>
--- artistproject/src/components/NavBar.jsx
+++ artistproject/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import projectLogo from "../assets/LOGO11.png";
+import projectLogo from "../assets/LOGO.png";
 import $ from "jquery";
 
 export default function NavBar() {
@@ -17,9 +17,23 @@ export default function NavBar() {
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container">
         <img className="projectLogo" src={projectLogo} alt="Logo"></img>
-        <a className="navbar-brand" href="#">
+        {/* <a className="navbar-brand" href="#">
           Artist Project Logo
-        </a>
+        </a> */}
+        <form className="d-flex position-relative" role="search">
+          <input
+            className="form-control ms-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+
+          <div className="d-flex align-items-center">
+            <span className="material-symbols-outlined position-absolute end-0 px-2">
+              search
+            </span>
+          </div>
+        </form>
         <button
           className="navbar-toggler"
           type="button"
@@ -35,7 +49,7 @@ export default function NavBar() {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
-                首頁
+                Home
               </a>
             </li>
             <li className="nav-item dropdown">
@@ -46,7 +60,7 @@ export default function NavBar() {
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                作家
+                Artists
               </a>
               <ul className="dropdown-menu">
                 <li>
@@ -71,39 +85,33 @@ export default function NavBar() {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                團隊
+                ArtWorks
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                登入
+                Auctions
               </a>
             </li>
-
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
           </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              <span className="material-symbols-outlined">search</span>
+          <div className="d-flex">
+            <button
+              type="button"
+              className="btn "
+              data-bs-toggle="modal"
+              data-bs-target="#LoginModal"
+            >
+              Log_In
             </button>
             <button
               type="button"
               className="btn btn-primary"
               data-bs-toggle="modal"
-              data-bs-target="#staticBackdrop"
+              data-bs-target="#"
             >
-              Log_In
+              SIGN UP
             </button>
-            <div className=""></div>
-          </form>
+          </div>
         </div>
       </div>
     </nav>
