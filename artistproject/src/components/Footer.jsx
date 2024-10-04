e4a801ad12f554e553f71efe8545264035cd11aa
YangYang890120 <ja24082010@gmail.com>
2024-10-04 17:27:40 +0800

    addfooter

---
+++ artistproject/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+
+import projectLogo from "../assets/LOGO.png";
+
+export default function NavBar() {
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container">
+        <div>
+          <img className="projectLogo  w-50 h-100"
+              src={projectLogo}
+              alt="Logo" />
+        </div>
+        <div className="aboutus">
+          <h2>About Us</h2>
+          
+        </div>
+        <div></div>
+      </div>
+    </nav>
+  );
+}
