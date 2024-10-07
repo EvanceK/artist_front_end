334e718180d1d80136c81de684b57a61f79cba46
YangYang890120 <ja24082010@gmail.com>
2024-10-07 23:02:02 +0800

    update

--- artistproject/src/components/Footer.jsx
+++ artistproject/src/components/Footer.jsx
@@ -4,16 +4,16 @@ export default function Footer() {
   return (
     <footer className="footer bg-light">
       <div className="container d-flex">
-        <div className="foot w-100 d-flex ">
-          <div className=" w-25 d-flex justify-content-center align-items-center">
+        <div className="foot w-100 d-flex row p-4">
+          <div className=" w-25 d-flex justify-content-center align-items-center col-3">
             <img
               className="projectLogo w-75 h-50 "
               src={projectLogo}
               alt="Logo"
             />
           </div>
-          <div className="aboutus w-20">
-            <div className="d-flex flex-column justify-content-center align-items-center">
+          <div className="aboutus w-20 col-3">
+            <div className="d-flex flex-column justify-content-center align-items-center ">
               <h1>About Us</h1>
               <span>Chen Chen</span>
               <span>Daniel</span>
@@ -21,8 +21,8 @@ export default function Footer() {
               <span>Wenyang</span>
             </div>
           </div>
-          <div className="Buy-group  w-25">
-            <div className="d-flex flex-column justify-content-center align-items-center">
+          <div className="Buy-group  w-25 col-3">
+            <div className="d-flex flex-column justify-content-center align-items-center ">
               <h1>Buy</h1>
               <span>How to buy</span>
               <span>Buyer terms</span>
