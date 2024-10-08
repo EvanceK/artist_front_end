164559d2ffe64ae6f4b9152e4218b0462b7f57c6
YangYang890120 <ja24082010@gmail.com>
2024-10-08 16:54:17 +0800

    updatemyaccount

--- artistproject/src/pages/MainPageComponents/MyAccount.jsx
+++ artistproject/src/pages/MainPageComponents/MyAccount.jsx
@@ -2,31 +2,40 @@ export default function MyAccount() {
   return (
     <>
       <div className="container">
-        <h2 className="my-5 py-5">My Account</h2>
-        <div className="row d-flex">
-          <div className="col-3"></div>
-          <div className="information col-3 d-flex ali">
-            <div className="d-flex flex-column justify-content-"> 
-              <h4>Name:</h4>
-              <h4>Nikename:</h4>
-              <h4>E-mail:</h4>
-              <h4>Phone:</h4>
-              <h4>Address:</h4>
-              <h4>Password:</h4>
-              <div className="btn m-4 w-100" id="edit">Edit</div>
+        <h2 className="my-5 py-5 border-bottom">My Account</h2>
+        <div className="row">
+          <div className="d-flex flex-column justify-content-center">
+            <div className="row d-flex justify-content-center">
+              <div className="col-2"><h4>Name:</h4></div>
+              <div className="col-4"><h4>Chan Yo Yo</h4></div>
             </div>
+            <div className="row d-flex justify-content-center">
+              <div className="col-2"><h4>Nikename:</h4></div>
+              <div className="col-4"><h4>YoYoYo</h4></div>
+            </div> 
+            <div className="row d-flex justify-content-center">
+              <div className="col-2"><h4>E-mail:</h4></div>
+              <div className="col-4"><h4>YoYoYo@gmail.com</h4></div>
+            </div> 
+            <div className="row d-flex justify-content-center">
+              <div className="col-2"><h4>Phone:</h4></div>
+              <div className="col-4"><h4>0937***182</h4></div>
+            </div> 
+            <div className="row d-flex justify-content-center">
+              <div className="col-2"><h4>Address:</h4></div>
+              <div className="col-4"><h4>236 NewTaipeiCity</h4></div>
+            </div> 
+            <div className="row d-flex justify-content-center">
+              <div className="col-2"><h4>Password:</h4></div>
+              <div className="col-4"><h4>chen24010812</h4></div>
             </div>
-            <div className="col-3">
-              <div>
-              <h4>Chan Yo Yo</h4>
-              <h4>YoYoYo</h4>
-              <h4>YoYoYo@gmail.com</h4>
-              <h4>0937***182</h4>
-              <h4>236 NewTaipeiCity</h4>
-              <h4>chen24010812</h4>
-              <div className="btn m-4" id="delete">Delete</div>
-              </div>
-            </div>      
+            <div className="row d-flex justify-content-center ">
+              <div className="btn m-4 col-1 " id="edit">Edit</div>        
+              <div className="col-1"></div>
+              <div className="btn btn-danger m-4 col-1" id="delete">Delete</div>      
+              <div className="col-2"></div>
+            </div>
+          </div>
         </div>
       </div>
     </>
