984cdf70986d173421d433c8564fdaff59792161
YangYang890120 <ja24082010@gmail.com>
2024-10-08 23:31:16 +0800

    0.0

--- artistproject/src/pages/MainPageComponents/MyAccount.jsx
+++ artistproject/src/pages/MainPageComponents/MyAccount.jsx
@@ -1,33 +1,36 @@
-export default function MyAccount() {
+import { useState } from "react";
+
+
+export default function MyAccount(costomer) {
+  //要帶入costomer的資料
   return (
-    <>
       <div className="container">
         <h2 className="my-5 py-5 border-bottom">My Account</h2>
         <div className="row">
           <div className="d-flex flex-column justify-content-center">
             <div className="row d-flex justify-content-center">
               <div className="col-2"><h4>Name:</h4></div>
-              <div className="col-4"><h4>Chan Yo Yo</h4></div>
+              <div className="col-4"><h4>{costomer.name}</h4></div>
             </div>
             <div className="row d-flex justify-content-center">
               <div className="col-2"><h4>Nikename:</h4></div>
-              <div className="col-4"><h4>YoYoYo</h4></div>
+              <div className="col-4"><h4>{costomer.nick_name}</h4></div>
             </div> 
             <div className="row d-flex justify-content-center">
               <div className="col-2"><h4>E-mail:</h4></div>
-              <div className="col-4"><h4>YoYoYo@gmail.com</h4></div>
+              <div className="col-4"><h4>{costomer.email}</h4></div>
             </div> 
             <div className="row d-flex justify-content-center">
               <div className="col-2"><h4>Phone:</h4></div>
-              <div className="col-4"><h4>0937***182</h4></div>
+              <div className="col-4"><h4>{costomer.phone}</h4></div>
             </div> 
             <div className="row d-flex justify-content-center">
               <div className="col-2"><h4>Address:</h4></div>
-              <div className="col-4"><h4>236 NewTaipeiCity</h4></div>
+              <div className="col-4"><h4>{costomer.address}</h4></div>
             </div> 
             <div className="row d-flex justify-content-center">
               <div className="col-2"><h4>Password:</h4></div>
-              <div className="col-4"><h4>chen24010812</h4></div>
+              <div className="col-4"><h4>{costomer.password}</h4></div>
             </div>
             <div className="row d-flex justify-content-center ">
               <div className="btn m-4 col-1 " id="edit">Edit</div>        
@@ -38,6 +41,5 @@ export default function MyAccount() {
           </div>
         </div>
       </div>
-    </>
   );
 }
