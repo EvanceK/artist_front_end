ca2f1fb52d7da630c287aa0fa69fb2cd182d55e2
Chenchen <evachen0812@gmail.com>
2024-10-17 22:02:48 +0800

    up

--- artistproject/src/pages/MainPageComponents/MyAccountComponents/MyWalletCard.jsx
+++ artistproject/src/pages/MainPageComponents/MyAccountComponents/MyWalletCard.jsx
@@ -1,12 +1,15 @@
 import PropTypes from "prop-types";
 
 export default function MywalletCard (MyWallCardProps){
+
+
     return(
         <div className="d-flex justify-content-between h5 mt-3 px-5">
             {MyWallCardProps.biddingHistory.map((historyItem, index) =>(
             <div key={index} className="d-flex justify-content-between w-100">
+            <h1>hello</h1>
             <span>{historyItem.refundDate}</span> 
-            <span>{historyItem.refundAmount}</span> {/* 假設你還想顯示 refundAmount */}
+            <span>{historyItem.refundAmount}</span>
           </div>
             ))}
             
@@ -16,9 +19,9 @@ export default function MywalletCard (MyWallCardProps){
 
 MywalletCard.propTypes = {
     MyWallCardProps: PropTypes.shape({
-      bankAccount: PropTypes.string.isRequired,
-      creditCardNo: PropTypes.string.isRequired,
-      bankBalance: PropTypes.number.isRequired, // 使用 number 而不是 Double
+      // bankAccount: PropTypes.string.isRequired,
+      // creditCardNo: PropTypes.string.isRequired,
+      // bankBalance: PropTypes.number.isRequired, // 使用 number 而不是 Double
       biddingHistory: PropTypes.arrayOf( // 定義 biddingHistory 的結構
         PropTypes.shape({
           refundDate: PropTypes.string.isRequired, // 假設 refundDate 是字符串
--- artistproject/src/pages/Resetpassword.jsx
+++ artistproject/src/pages/Resetpassword.jsx
@@ -1,9 +1,14 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom"
 
 export default function Resetpassword(){
-
+  const path = import.meta.env.VITE_DATA_HOST_API;
+  const api = path + "/verifyResetUrl";
   const params= useParams();
- 
+ useEffect(async()=>{
+    const result=await axios.get(`${api}`);
+ },[])
     return(
         <div>
             <h1>hello</h1>
