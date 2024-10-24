8fa1b4ebf7ba12e6f1e7f93bfdd2c096f12e7318
DannyTan8x <dannytan8xtw@icloud.com>
2024-10-24 12:08:53 +0800

    update Order & delivery flow

--- artistproject/src/components/ConfirmOrderCustomerInfo.jsx
+++ artistproject/src/components/ConfirmOrderCustomerInfo.jsx
@@ -6,8 +6,8 @@ function ConfirmOrderCustomerInfo() {
       <div className=" h1 border w-75 mb-5">
         <div className=" mt-5 ms-5 me-5">
           <CustomerInfo />
-        
-        {/*Payment imformation */}
+
+          {/* Payment imformation
         <div className="h1 underline mt-5">Payment </div>
         <div className="mt-5">
           <div className=" flex-column h3">
@@ -15,22 +15,22 @@ function ConfirmOrderCustomerInfo() {
             <p>Credit Card Number: 1234-1234-1234-1234</p>
             <p>Credit Card Installments: 3 months</p>
           </div>
-        </div>
-       
-        {/*Recipient imformation */}
-        <div className="h1 underline mt-5">Recipient information </div>
-        <div className="mt-5">
-          <div className=" flex-column h3">
-            <p>Recipient's Name: Chen Yo Yo</p>
-            <p>Phone: 1234</p>
-            <p>Address: 236 NewTaipeiCity</p>
-            <p>E-mail: yoyoyo394@gmail.com</p>
-            <p>Shipping Method: Delivery</p>
+        </div> */}
+
+          {/*Recipient imformation */}
+          <div className="h1 underline mt-5">Recipient information </div>
+          <div className="mt-5">
+            <div className=" flex-column h3">
+              <p>Recipient's Name: Chen Yo Yo</p>
+              <p>Phone: 1234</p>
+              <p>Address: 236 NewTaipeiCity</p>
+              <p>E-mail: yoyoyo394@gmail.com</p>
+              <p>Shipping Method: Delivery</p>
+            </div>
           </div>
         </div>
-        </div>
       </div>
-      </div>
+    </div>
   );
 }
 export default ConfirmOrderCustomerInfo;
--- artistproject/src/pages/MainPageComponents/StaffDashBoardpages/PaintingfindAll.jsx
+++ artistproject/src/pages/MainPageComponents/StaffDashBoardpages/PaintingfindAll.jsx
@@ -11,13 +11,13 @@ export default function PaintingfindAll() {
   const [uploadToggle, setUploadToggle] = useState(false);
   const formData = new FormData();
   //下面是翻頁用
-  const[url,setUrl]=useState()
+  const [url, setUrl] = useState();
   const [data, setData] = useState([]);
   const [totalPage, setTotalPage] = useState(1);
   const [requestPageNumber, setRequestPageNumber] = useState(1);
   //上面是翻頁用
   const [inputData, setInputData] = useState();
-  const [readData, setReadData] = useState();//儲存edit的data並顯示在inputbox
+  const [readData, setReadData] = useState(); //儲存edit的data並顯示在inputbox
   const [paintingData, setPaintingData] = useState();
   const [paintingTable, setPaintingTable] = useState();
   // methods for loading data
@@ -26,7 +26,7 @@ export default function PaintingfindAll() {
     handleSubmit, //submit action
     watch, //watching form control change
     setValue, //set value from watched control
-    reset,//resetForm
+    reset, //resetForm
   } = useForm();
 
   const getArtistList = async () => {
@@ -45,9 +45,8 @@ export default function PaintingfindAll() {
     // 等同 $.ajax(" get blablablba ")
     try {
       const result = await axiosInstance.get(`${api}`);
-      // console.log(result);
-      setPaintingData(result.data)
-
+      console.log(result);
+      setPaintingData(result.data);
     } catch (error) {
       console.log(error);
     }
@@ -73,8 +72,8 @@ export default function PaintingfindAll() {
     setSelectedOption(event.target.value);
     console.log(event.target.value);
   };
-  const getPaintdata = async (event)=>{
-    const id = event.target.id
+  const getPaintdata = async (event) => {
+    const id = event.target.id;
     console.log(id);
     const api = path + "/PTController/findpaintingid/";
     // 等同 $.ajax(" get blablablba ")
@@ -85,16 +84,16 @@ export default function PaintingfindAll() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    if(readData){
-      document.getElementById("artistId").value=readData.artistId
-      setValue("artistId",readData.artistId)
-      document.getElementById("paintingId").value=readData.paintingId
-      setValue("paintingId",readData.paintingId)
-      document.getElementById("paintingName").value=readData.paintingName
-      setValue("paintingName",readData.paintingName)
+  useEffect(() => {
+    if (readData) {
+      document.getElementById("artistId").value = readData.artistId;
+      setValue("artistId", readData.artistId);
+      document.getElementById("paintingId").value = readData.paintingId;
+      setValue("paintingId", readData.paintingId);
+      document.getElementById("paintingName").value = readData.paintingName;
+      setValue("paintingName", readData.paintingName);
       document.getElementById("date").value = readData.date;
       setValue("date", readData.date);
       document.getElementById("style").value = readData.style;
@@ -108,50 +107,49 @@ export default function PaintingfindAll() {
       document.getElementById("status").value = readData.status;
       setValue("status", readData.status);
     }
-  },[readData])
+  }, [readData]);
 
-const createPainting= async()=>{
-  try{
-  
-    for (const key in inputData) {
-      formData.append(key, inputData[key]);
+  const createPainting = async () => {
+    try {
+      for (const key in inputData) {
+        formData.append(key, inputData[key]);
+      }
+      console.log(formData);
+      const api = path + "/PTController/createPainting";
+      const result = await axiosInstance.post(api, formData, {
+        headers: { "Content-Type": "multpart/form-data" },
+      });
+      console.log(result.data);
+      setUploadToggle(!uploadToggle);
+      reset();
+      alert("新增成功");
+    } catch (error) {
+      console.log(error);
     }
-    console.log(formData);
-    const api = path + "/PTController/createPainting";
-    const result = await axiosInstance.post(api,formData,{
-      headers:{ "Content-Type":"multpart/form-data"}
-    })
-    console.log(result.data);
-    setUploadToggle(!uploadToggle);
-    reset();
-    alert("新增成功")
-  }catch(error){
-    console.log(error);
-  }
-};
-// useEffect(()=>{
+  };
+  // useEffect(()=>{
 
-// },[uploadToggle])
-const updataPainting = async ()=>{
-  try{
-    console.log(inputData);
-    const api = path + "/PTController/editPainting";
-    const result = await axiosInstance.put(api,inputData);
-    console.log(result.data);
-    setUploadToggle(!uploadToggle);
-    reset();
-    alert("修改成功")
-  }catch(error){
-    console.log(error);
-  }
-};
+  // },[uploadToggle])
+  const updataPainting = async () => {
+    try {
+      console.log(inputData);
+      const api = path + "/PTController/editPainting";
+      const result = await axiosInstance.put(api, inputData);
+      console.log(result.data);
+      setUploadToggle(!uploadToggle);
+      reset();
+      alert("修改成功");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   //將資料寫進inputdata
   const onSubmit = (data) => {
     //確認資料
-    if(data.confirmed){
-     console.log(data);
-     setInputData(data);
-    }else{
+    if (data.confirmed) {
+      console.log(data);
+      setInputData(data);
+    } else {
       alert("Please Confirmed");
     }
   };
@@ -159,25 +157,25 @@ const updataPainting = async ()=>{
     setInputData((prev) => ({ ...prev, image: e.target.files[0] }));
   };
   //判斷資料是創建或修改
-  useEffect(()=>{
-    if(inputData)
-    try {
-      //確認有沒有id
-     if(inputData.paintingId==""){
-      if(inputData.paintingName&&inputData.artistId){
-        createPainting();
-      }else{
-        alert("欄位不可為空");
-      };
-     }else{
-      // readData.date=inputData.date
-      // setReadData(inputData)
-      updataPainting();
-     }
-    } catch (error) {
-      console.log(error);
-    }
-  },[inputData])
+  useEffect(() => {
+    if (inputData)
+      try {
+        //確認有沒有id
+        if (inputData.paintingId == "") {
+          if (inputData.paintingName && inputData.artistId) {
+            createPainting();
+          } else {
+            alert("欄位不可為空");
+          }
+        } else {
+          // readData.date=inputData.date
+          // setReadData(inputData)
+          updataPainting();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+  }, [inputData]);
   //用artistID取得畫作資料
   const getdata = useCallback(async () => {
     const api = path + "/PTController/artists";
@@ -197,35 +195,40 @@ const updataPainting = async ()=>{
 
   useEffect(() => {
     getdata();
-    setValue("artistId",selectedOption)
-  }, [selectedOption,uploadToggle,requestPageNumber]);
+    setValue("artistId", selectedOption);
+  }, [selectedOption, uploadToggle, requestPageNumber]);
   //將資料寫進table
   useEffect(() => {
     if (paintingData) setPaintingTable(buildArtistTable());
   }, [paintingData]);
   //刪除按鈕
-  const deletePaintdata = async(event)=>{
-    const id = event.target.id
+  const deletePaintdata = async (event) => {
+    const id = event.target.id;
     console.log(id);
     const api = path + "/PTController/";
     // 等同 $.ajax(" get blablablba ")
     try {
       const result = await axiosInstance.delete(`${api}${id}`);
       console.log(result.data);
-      setUploadToggle(!uploadToggle)
-      alert("刪除成功")
+      setUploadToggle(!uploadToggle);
+      alert("刪除成功");
     } catch (error) {
-      console.log("刪除失敗"+error);
-
+      console.log("刪除失敗" + error);
     }
-  }
+  };
   const buildArtistTable = () => {
     return paintingData.map((a, i) => {
       console.log(a);
       return (
         <tr key={i}>
           <th scope="row">{a.paintingId}</th>
-          <td><img className="img-thumbnail"src={`data:image/jpeg;base64,${a.image}`}  alt="Photo"/></td>
+          <td>
+            <img
+              className="img-thumbnail"
+              src={`data:image/jpeg;base64,${a.image}`}
+              alt="Photo"
+            />
+          </td>
           <td>{a.paintingName}</td>
           <td className="text-end">
             {new Intl.NumberFormat("en-IN", {
@@ -235,13 +238,21 @@ const updataPainting = async ()=>{
           <td>{a.uploadDate}</td>
           <td>{a.delicated}</td>
           <td>{a.status}</td>
-         
+
           <td className="d-flex align-items-center justify-conten-center">
             <div className="row d-flex ">
-              <div className="btn col-5" id={a.paintingId} onClick={getPaintdata}>
+              <div
+                className="btn col-5"
+                id={a.paintingId}
+                onClick={getPaintdata}
+              >
                 Edit
               </div>
-              <div className="btn btn-danger col-5" id={a.paintingId} onClick={deletePaintdata}>
+              <div
+                className="btn btn-danger col-5"
+                id={a.paintingId}
+                onClick={deletePaintdata}
+              >
                 Delete
               </div>
             </div>
@@ -311,50 +322,83 @@ const updataPainting = async ()=>{
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="Photo" className="form-label" >
+            <label htmlFor="Photo" className="form-label">
               Photo
             </label>
-            <input type="file" className="form-control" id="Photo" 
-            // {...register("image")}
-            onChange={handleFileChange}
+            <input
+              type="file"
+              className="form-control"
+              id="Photo"
+              // {...register("image")}
+              onChange={handleFileChange}
             />
           </div>
           <div className="mb-3">
             <label htmlFor="date" className="form-label">
               Date
             </label>
-            <input type="text" className="form-control" id="date"{...register("date")} />
+            <input
+              type="text"
+              className="form-control"
+              id="date"
+              {...register("date")}
+            />
           </div>
 
           <div className="mb-3">
             <label htmlFor="style" className="form-label">
               Style
             </label>
-            <input type="text" className="form-control" id="style" {...register("style")}/>
+            <input
+              type="text"
+              className="form-control"
+              id="style"
+              {...register("style")}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="genre" className="form-label">
               Genre
             </label>
-            <input type="text" className="form-control" id="genre"{...register("genre")} />
+            <input
+              type="text"
+              className="form-control"
+              id="genre"
+              {...register("genre")}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="price" className="form-label">
               Price
             </label>
-            <input type="text" className="form-control" id="price" {...register("price")}/>
+            <input
+              type="text"
+              className="form-control"
+              id="price"
+              {...register("price")}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="delicated" className="form-label">
-            Delicated
+              Delicated
             </label>
-            <input type="text" className="form-control" id="delicated" {...register("delicated")}/>
+            <input
+              type="text"
+              className="form-control"
+              id="delicated"
+              {...register("delicated")}
+            />
           </div>
           <div className="mb-3">
             <label htmlFor="status" className="form-label">
-            Status
+              Status
             </label>
-            <input type="text" className="form-control" id="status" {...register("status")}/>
+            <input
+              type="text"
+              className="form-control"
+              id="status"
+              {...register("status")}
+            />
           </div>
           <div className="mb-3 form-check">
             <input
@@ -367,7 +411,7 @@ const updataPainting = async ()=>{
               Confirmed
             </label>
           </div>
-          <button type="submit" className="btn btn-primary" >
+          <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </form>
@@ -376,11 +420,11 @@ const updataPainting = async ()=>{
           className="table-responsive col-8"
           style={{ maxHeight: "80vh", overflowY: "auto" }}
         >
-            <Pagination
-              totalPage={totalPage}
-              requestPageNumber={requestPageNumber}
-              onPageChange={setRequestPageNumber}
-            />
+          <Pagination
+            totalPage={totalPage}
+            requestPageNumber={requestPageNumber}
+            onPageChange={setRequestPageNumber}
+          />
           <table className="table table-hover">
             <thead>
               <tr>
@@ -399,10 +443,10 @@ const updataPainting = async ()=>{
             </tbody>
           </table>
           <Pagination
-              totalPage={totalPage}
-              requestPageNumber={requestPageNumber}
-              onPageChange={setRequestPageNumber}
-            />
+            totalPage={totalPage}
+            requestPageNumber={requestPageNumber}
+            onPageChange={setRequestPageNumber}
+          />
         </div>
       </div>
     </>
